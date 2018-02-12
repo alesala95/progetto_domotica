aee9709be7d3cb8fb386b7bc97ae8abe74a02a4b
1ITS-master\itsadmin <aletchoukball95@gmail.com>
2018-02-12 17:56:31 +0100

    no message

--- progetto_domotica_jpa/src/it/progetto_domotica/business/JPAUtility.java
+++
@@ -1,9 +0,0 @@
-package it.progetto_domotica.business;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-public class JPAUtility {
-
-	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("domotica");
-}
