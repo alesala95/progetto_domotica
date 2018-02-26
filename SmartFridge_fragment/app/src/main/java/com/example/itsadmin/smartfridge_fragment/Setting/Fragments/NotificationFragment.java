aa03fdf3f3b0fe4d3aff2c04984f82735bc81cc3
luca punzo <l.punzo@itsrizzoli.it>
2018-02-26 14:10:01 +0100

    modifiche layout

--- SmartFridge_fragment/app/src/main/java/com/example/itsadmin/smartfridge_fragment/Setting/Fragments/NotificationFragment.java
+++ SmartFridge_fragment/app/src/main/java/com/example/itsadmin/smartfridge_fragment/Setting/Fragments/NotificationFragment.java
@@ -94,7 +94,7 @@ public class NotificationFragment extends Fragment {
                 }
             }
         });
-        
+
 
 
         linearLayoutVibrationFridge.setOnClickListener(new View.OnClickListener() {
